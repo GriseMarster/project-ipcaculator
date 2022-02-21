b54f06e45e3861efb4faacfb2bde199d72659a3e
GriseMarster <ttanghus@gmail.com>
2022-02-21 07:41:30 +0100

    minor change

--- caculator.py
+++ caculator.py
@@ -5,11 +5,11 @@ from typing import ContextManager
 
 
 while True:
-    ipa = input('\n' 'Wanna see the computers IP(1)\nSet a custom IP(2)\nQuit(3): ')
+    ipAdressing = input('\n' 'Wanna see the computers IP(1)\nSet a custom IP(2)\nQuit(3): ')
 
     # takes the output of "ipa" and if it's 1 it finds the computers details like it's name and ip address.
     # needs to add more later
-    if ipa == '1':
+    if ipAdressing == '1':
         hName = socket.gethostname()
         IPaddress = socket.gethostbyname(hName)
         Class_conveter = (IPaddress[:3])
@@ -27,12 +27,19 @@ while True:
         elif BClass in range(192, 223, 1):
             print('Class is: C')
         
+        elif BClass in range(224, 239, 1):
+            print('Class is: D')
+
+        elif BClass in range(240, 255, 1):
+            print('Class is: E')
+        
         else:
-          print("lol didn't work ")
+          print("Not at correct ip")
+          continue
 
 
     # takes the output of "ipa" and if it's 2 it tries to show the subnet, subnet mask, brodcast ip, id address, classes. 
-    elif ipa == '2':
+    elif ipAdressing == '2':
         # user enters the ip address here, and if there is added a subnet it needs to take that in consiterration
         
         input2 = (input('Insert IP address: '))# user needs to insert a custom ip address
@@ -40,6 +47,7 @@ while True:
         BClass = int(Class_conveter)
         IPB = input2.split('.')
         
+        # Checks the users input to see what class the ip-address is under.
         print('Your IP is: ' + input2)
         if BClass in range(1, 126, 1):
             print('Class is: A')
@@ -62,18 +70,19 @@ while True:
             print(IPB)
 
         else:
-          print("lol didn't work ")
+          print("Not an correct ip")
+          continue
 
-    elif ipa == 'secret word':
+    elif ipAdressing == 'secret word':
         x = input('what do you wanna do?: ')
         subprocess.run(x)
     
-    elif ipa == '3':
+    elif ipAdressing == '3':
         print('Quitting...')
         break
 
     else:
-        # if the user doesn't chosses between 1 or 2 they will get and error that they have to chosse between 1 and 2
+        # if the user doesn't chosses between 1, 2 or 3 they will get and error that they have to chosse a correct number
         print('\n' 'incorrect number, you need to chosse between 1 to 3.')
         continue
 
